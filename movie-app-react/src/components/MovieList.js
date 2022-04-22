5bc8171448d5e265943456cefb1893f9a0605545
scarperez7 <95876497+scarperez7@users.noreply.github.com>
2022-04-21 21:33:08 -0400

    added overlay and updated styling

--- movie-app-react/src/components/MovieList.js
+++ movie-app-react/src/components/MovieList.js
@@ -4,7 +4,12 @@ const MovieList = (props) => {
   return (
     <div className="d-flex justify-content-start ">
       {props.movies.map((movie, index) => (
-        <img src={movie.Poster} alt="movie" className="m-3" />
+        <div className="image-container m-4">
+          <img src={movie.Poster} alt="movie" className="m-3" />
+          <div className="overlay d-flex align-items-center justify-content-center">
+            Add to Favourites
+          </div>
+        </div>
       ))}
       ;
     </div>
